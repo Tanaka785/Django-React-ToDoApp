d32d1cf8de42fca7eed1e739b4340a11d79c7aab
Tanaka785 <tanakachanetsa204@gmail.com>
2025-02-09 00:50:21 +0200

    adds files

---
+++ frontend/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "./static/frontend"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  optimization: {
+    minimize: process.env.NODE_ENV === "production", // Minimize only in production
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify(
+        process.env.NODE_ENV || "development"
+      ), // Default to 'development' if not set
+    }),
+  ],
+};
