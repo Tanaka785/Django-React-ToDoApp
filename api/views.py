4845f0b4878cb848509dec0bb5d04cb12fe91f5b
Tanaka785 <tanakachanetsa204@gmail.com>
2025-02-08 08:39:57 +0200

    creates taskserializer view

--- api/views.py
+++ api/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
-from django.http import render 
+from rest_framework import generics, status 
+from .models import Task 
+from .serializers import TaskSerializer 
+from rest_framework.views import APIView
+
 
 # Create your views here.
+class TaskSerializer(generics.ListAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
+class CreateTask(APIView):
+    ...
