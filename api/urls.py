46e94fc41aeef1bd2db7a3baa167f24f2ba2f698
Tanaka785 <tanakachanetsa204@gmail.com>
2025-02-08 09:00:14 +0200

    adds two urlpatterns

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = "frontend"
 
 urlpatterns = [
-    path('/create', views.CreateTaskView.as_view()),
+    path('tasks', views.TaskSerializer.as_view(), name="create-task"),
+    path('create-task', views.CreateTask.as_view(), name="create-task"),
 ]
