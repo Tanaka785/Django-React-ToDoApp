6e67ec5319bd93e46b32dfe60f83284eba9c4f95
Tanaka785 <tanakachanetsa204@gmail.com>
2025-02-10 00:04:11 +0200

    removes other fields

--- api/models.py
+++ api/models.py
@@ -3,6 +3,4 @@ from django.db import models
 # Create your models here.
 class Task(models.Model):
     title = models.CharField(max_length=50)
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
-    description = models.CharField(max_length=127, null=True, blank=True)
+    
