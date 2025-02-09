20847d3a409672e3cb83b0dde5b7e49f4461b47f
Tanaka785 <tanakachanetsa204@gmail.com>
2025-02-09 13:04:46 +0200

    changes the div to box, and corrects the root rendering

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,21 +1,24 @@
-import React, { Component } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
+import { Box } from "@mui/material";
 import Homepage from "./Homepage";
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="center" style={{ border: '1px solid blue'}}>
-        <Homepage />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <Box
+      className="center"
+      sx={{
+        border: "1px solid",
+        borderColor: "primary.main",
+        borderRadius: "4px",
+        boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.2)",
+      }}
+    >
+      <Homepage />
+    </Box>
+  );
 }
 
 const appDiv = document.getElementById("app");
 const root = createRoot(appDiv);
-root.render(<App />, appDiv);
+root.render(<App />);
