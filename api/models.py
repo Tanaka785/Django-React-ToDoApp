db9ab5c0e56285d2c68529d9aeafc797b50102f4
Tanaka785 <tanakachanetsa204@gmail.com>
2025-02-10 12:04:11 +0200

    adds Meta class

--- api/models.py
+++ api/models.py
@@ -6,5 +6,8 @@ class Task(models.Model):
     title = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
     
+    class Meta: 
+        ordering = ["-created_at"]
+        
     def __str__(self):
         return (self.title)
