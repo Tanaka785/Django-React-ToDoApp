812b7650b1b0644d087910674ec449b8c0c47a67
Tanaka785 <tanakachanetsa204@gmail.com>
2025-02-10 12:11:43 +0200

    increases the max length of the title field

--- api/models.py
+++ api/models.py
@@ -3,11 +3,11 @@ from django.utils import timezone
 
 # Create your models here.
 class Task(models.Model):
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=176)
     created_at = models.DateTimeField(auto_now_add=True)
     
     class Meta: 
         ordering = ["-created_at"]
-        
+
     def __str__(self):
         return (self.title)
