d2fe48f1576b359756f210013fe2688ab9250a87
Tanaka785 <tanakachanetsa204@gmail.com>
2025-02-10 12:02:37 +0200

    adds field "created_at"

--- api/models.py
+++ api/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Task(models.Model):
     title = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
     
