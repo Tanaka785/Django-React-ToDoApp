f90c9b3869d13bc7380658841bfbb0f24746fc74
Tanaka785 <tanakachanetsa204@gmail.com>
2025-02-10 12:03:21 +0200

    defines the __str__ method

--- api/models.py
+++ api/models.py
@@ -6,3 +6,5 @@ class Task(models.Model):
     title = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
     
+    def __str__(self):
+        return (self.title)
