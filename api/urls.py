58acee00f9b980ac81250677395a009c55be64cd
Tanaka785 <tanakachanetsa204@gmail.com>
2025-02-11 14:53:19 +0200

    adds "update-task" url

--- api/urls.py
+++ api/urls.py
@@ -6,4 +6,5 @@ app_name = "api"
 urlpatterns = [
     path('tasks', views.TaskSerializerView.as_view(), name="tasks"),
     path('create-task', views.CreateTask.as_view(), name="create-task"),
+    path('<int:task_id>/update', views.UpdateTask.as_view(), name="update-task"),
 ]
