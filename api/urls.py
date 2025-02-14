336f04e99bca5def2f6d5ca32aa2980877a64afc
Tanaka785 <tanakachanetsa204@gmail.com>
2025-02-14 15:38:16 +0200

    removes urls

--- api/urls.py
+++ api/urls.py
@@ -5,8 +5,6 @@ app_name = "api"
 
 urlpatterns = [
     path('tasks', views.TaskSerializerView.as_view(), name="tasks"),
-    path('tasks/active', views.GetActiveTasks.as_view(), name="active-tasks"),
-    path('tasks/completed', views.GetCompletedTasks.as_view(), name="completed-tasks"),
     path('create-task', views.CreateTask.as_view(), name="create-task"),
-    path('update-task', views.UpdateTask.as_view(), name="update-task"),
+    path('<int:task_id>/update', views.UpdateTask.as_view(), name="update-task"),
 ]
