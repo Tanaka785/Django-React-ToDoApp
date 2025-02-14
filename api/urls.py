c8d2f676a50fc7a80d570c4da9f1aac6c881f943
Tanaka785 <tanakachanetsa204@gmail.com>
2025-02-14 15:28:14 +0200

    adds urls to get active and to get completed tasks

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,8 @@ app_name = "api"
 
 urlpatterns = [
     path('tasks', views.TaskSerializerView.as_view(), name="tasks"),
+    path('tasks/active', views.GetActiveTasks.as_view(), name="active-tasks"),
+    path('tasks/completed', views.GetCompletedTasks.as_view(), name="completed-tasks"),
     path('create-task', views.CreateTask.as_view(), name="create-task"),
     path('update-task', views.UpdateTask.as_view(), name="update-task"),
 ]
