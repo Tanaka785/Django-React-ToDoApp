210900e7a9ccaf275910bd94519dc2ef4ca74ee1
Tanaka785 <tanakachanetsa204@gmail.com>
2025-02-15 09:20:50 +0200

    replaces "width: 100%" with fullWidth attribute

--- frontend/src/components/EditTaskPage.js
+++ frontend/src/components/EditTaskPage.js
@@ -15,16 +15,14 @@ export default class EditTaskPage extends Component {
   render() {
     return (
       <Grid2 container spacing={1} direction={"column"} sx={{ width: "100%" }}>
-        <Grid2 xs={12}
-          sx={{ width: "100%" }}
-        >
+        <Grid2 fullWidth xs={12}>
           <form>
-            <Grid2 sx={{ width: "100%"}}>
-              <FormControl sx={{ width: '100%', marginBottom: '10px'}}>
-                <TextField required={true}></TextField>
+            <Grid2 fullWidth>
+              <FormControl fullWidth sx={{ marginBottom: "10px" }}>
+                <TextField required={true} variant="outlined"></TextField>
               </FormControl>
             </Grid2>
-            <Grid2 sx={{ width: "100%", textAlign: 'center'}}>
+            <Grid2 sx={{ textAlign: "center" }}>
               <Button type="submit" variant="disabled" color="primary">
                 Update
               </Button>
