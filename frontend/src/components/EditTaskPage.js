47273cbc68826a3539b3c4dad94dbe502dcf6d51
Tanaka785 <tanakachanetsa204@gmail.com>
2025-02-15 11:12:09 +0200

    implements functionality to fetch task

--- frontend/src/components/EditTaskPage.js
+++ frontend/src/components/EditTaskPage.js
@@ -6,29 +6,33 @@ import {
   Button,
 } from "@mui/material";
 import { useParams } from "react-router-dom";
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
 
 function EditTaskPage() {
+  const params = useParams();
+  const [state, setState] = useState({
+    taskId: null,
+  });
 
-  const taskId = useParams();
-  
-  // function that checks if the task to be edited exists.
-  const CheckIfTaskExits = () => {
-    const requestOptions = {
-      method: "GET",
-      headers: { "Content-Type": 'application/json' },
-      body: JSON.stringify({
-        id: taskId
-      })
-    }
-  }
+  // rerenders the component everytime it receives a new task.
+  useEffect(() => {
+    fetch(`/api/get-task?id=${params.taskId}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        // setState({
+        //   ...state,
+        //   taskId: data.id
+        // })
+      });
+  }, [params.taskId]);
 
   return (
     <Grid2 container spacing={1} direction={"column"} sx={{ width: "100%" }}>
-      <Grid2 sx={{ width: '100%'}} xs={12}>
+      <Grid2 sx={{ width: "100%" }} xs={12}>
         <form>
-          <Grid2 sx={{ width: '100%'}}>
-            <FormControl sx={{ width: '100%', marginBottom: "10px" }}>
+          <Grid2 sx={{ width: "100%" }}>
+            <FormControl sx={{ width: "100%", marginBottom: "10px" }}>
               <TextField required={true} variant="standard"></TextField>
             </FormControl>
           </Grid2>
