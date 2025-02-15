d9b08a01e4ec36a0245d3dd880cbc6e543a736aa
Tanaka785 <tanakachanetsa204@gmail.com>
2025-02-15 07:23:28 +0200

    sets up the initial setup of the component

--- frontend/src/components/Tasks.js
+++ frontend/src/components/Tasks.js
@@ -1,7 +1,20 @@
+import { Typography, Grid2 } from "@mui/material";
 import React, { Component } from "react";
 
 export default class Tasks extends Component {
     constructor(props) {
         super(props);
+    }
+
+    render() {
+        return (
+            <Grid2 container spacing={1} direction={"column"}>
+                <Grid2>
+                    <Typography>
+                        Active 
+                    </Typography>
+                </Grid2>
+            </Grid2>
+        )
     }
 }
