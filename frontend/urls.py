4da08ba7235aef319237731e3b3dc2fc6b3a171f
Tanaka785 <tanakachanetsa204@gmail.com>
2025-02-15 08:29:16 +0200

    adds new url

--- frontend/urls.py
+++ frontend/urls.py
@@ -6,5 +6,6 @@ app_name = "frontend"
 urlpatterns = [
     path("", views.index, name="index"),
     path("create", views.index, name="create-task"),
-    path("tasks/active", views.index, name="active-tasks")
+    path("tasks/active", views.index, name="active-tasks"),
+    path("tasks/<int:task_id>/edit", views.index, name="edit-task")
 ]
