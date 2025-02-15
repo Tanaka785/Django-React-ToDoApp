177c519bf48f386d54fcbece632bc585900b503f
Tanaka785 <tanakachanetsa204@gmail.com>
2025-02-15 09:51:53 +0200

    converts component to a function based component

--- frontend/src/components/EditTaskPage.js
+++ frontend/src/components/EditTaskPage.js
@@ -7,29 +7,25 @@ import {
 } from "@mui/material";
 import React, { Component } from "react";
 
-export default class EditTaskPage extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <Grid2 container spacing={1} direction={"column"} sx={{ width: "100%" }}>
-        <Grid2 fullWidth xs={12}>
-          <form>
-            <Grid2 fullWidth>
-              <FormControl fullWidth sx={{ marginBottom: "10px" }}>
-                <TextField required={true} variant="standard"></TextField>
-              </FormControl>
-            </Grid2>
-            <Grid2 sx={{ textAlign: "center" }}>
-              <Button type="submit" variant="disabled" color="primary">
-                Update
-              </Button>
-            </Grid2>
-          </form>
-        </Grid2>
+function EditTaskPage() {
+  return (
+    <Grid2 container spacing={1} direction={"column"} sx={{ width: "100%" }}>
+      <Grid2 fullWidth xs={12}>
+        <form>
+          <Grid2 fullWidth>
+            <FormControl fullWidth sx={{ marginBottom: "10px" }}>
+              <TextField required={true} variant="standard"></TextField>
+            </FormControl>
+          </Grid2>
+          <Grid2 sx={{ textAlign: "center" }}>
+            <Button type="submit" variant="disabled" color="primary">
+              Update
+            </Button>
+          </Grid2>
+        </form>
       </Grid2>
-    );
-  }
+    </Grid2>
+  );
 }
+
+export default EditTaskPage;
