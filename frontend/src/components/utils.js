68a00da3bd8a88f1d1c0cf68c171039638700eed
Tanaka785 <tanakachanetsa204@gmail.com>
2025-02-16 13:17:47 +0200

    makes the function an async function

--- frontend/src/components/utils.js
+++ frontend/src/components/utils.js
@@ -1,5 +1,8 @@
-export function getTasks() {
+// get tasks from api endpoint.
+export async function getTasks() {
   return fetch("/api/tasks")
     .then((response) => response.json())
     .then((data) => data);
 }
+
+// arranges the tasks.
