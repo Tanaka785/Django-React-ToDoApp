90cf6fefc7f54fb044733f937743412b6fe21c03
Tanaka785 <tanakachanetsa204@gmail.com>
2025-02-16 14:22:50 +0200

    adds home link to the page

--- frontend/src/components/Tasks.js
+++ frontend/src/components/Tasks.js
@@ -1,6 +1,7 @@
 import { Typography, Grid2 } from "@mui/material";
 import React, { Component } from "react";
 import { getTasks } from "./utils";
+import { Link } from "react-router-dom";
 
 export default class Tasks extends Component {
   constructor(props) {
@@ -15,6 +16,9 @@ export default class Tasks extends Component {
       <Grid2 container spacing={1} direction={"column"} sx={{ width: "100%" }}>
         <Grid2 xs={12} sx={{ textAlign: "center" }}>
           <Typography variant="h4">{this.state.title}</Typography>
+        </Grid2>
+        <Grid2 xs={12} sx={{ textAlign: "center" }}>
+          <Typography variant="h6" component={Link} to="/">Home</Typography>
         </Grid2>
       </Grid2>
     );
