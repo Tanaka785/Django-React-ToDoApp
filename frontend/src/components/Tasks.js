f8fc407878f57dbc6a9556f08cc0219d16a2357a
Tanaka785 <tanakachanetsa204@gmail.com>
2025-02-16 14:36:42 +0200

    converts component to a function based

--- frontend/src/components/Tasks.js
+++ frontend/src/components/Tasks.js
@@ -1,26 +1,24 @@
 import { Typography, Grid2 } from "@mui/material";
-import React, { Component, useEffect } from "react";
+import React, { Component, useEffect, useState } from "react";
 import { getTasks } from "./utils";
 import { Link } from "react-router-dom";
 
-export default class Tasks extends Component {
-  constructor(props) {
-      super(props);
-      this.state = {
-          title: "Active",
-      }
-  }
-  
-  render() {
-    return (
-      <Grid2 container spacing={1} direction={"column"} sx={{ width: "100%" }}>
-        <Grid2 xs={12} sx={{ textAlign: "center" }}>
-          <Typography variant="h4">{this.state.title}</Typography>
-        </Grid2>
-        <Grid2 xs={12} sx={{ textAlign: "center" }}>
-          <Typography variant="h6" component={Link} to="/">Home</Typography>
-        </Grid2>
+function Tasks() {
+  const [state, setState] = useState({
+    title: "Active",
+  });
+  return (
+    <Grid2 container spacing={1} direction={"column"} sx={{ width: "100%" }}>
+      <Grid2 xs={12} sx={{ textAlign: "center" }}>
+        <Typography variant="h4">{state.title}</Typography>
       </Grid2>
-    );
-  }
+      <Grid2 xs={12} sx={{ textAlign: "center" }}>
+        <Typography variant="h6" component={Link} to="/">
+          Home
+        </Typography>
+      </Grid2>
+    </Grid2>
+  );
 }
+
+export default Tasks;
