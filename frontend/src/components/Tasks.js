ba0fec7ec79f4f3134e307cb07a9e23f044591ff
Tanaka785 <tanakachanetsa204@gmail.com>
2025-02-16 12:18:19 +0200

    defines state variable title

--- frontend/src/components/Tasks.js
+++ frontend/src/components/Tasks.js
@@ -3,14 +3,17 @@ import React, { Component } from "react";
 
 export default class Tasks extends Component {
   constructor(props) {
-    super(props);
+      super(props);
+      this.state = {
+          title: "Active",
+      }
   }
 
   render() {
     return (
       <Grid2 container spacing={1} direction={"column"} sx={{ width: "100%" }}>
         <Grid2 xs={12} sx={{ textAlign: "center" }}>
-          <Typography variant="h4">Active</Typography>
+          <Typography variant="h4">{this.state.title}</Typography>
         </Grid2>
       </Grid2>
     );
