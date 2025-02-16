8f096b71eedd59b61e8c09a78c72de4ad3880b42
Tanaka785 <tanakachanetsa204@gmail.com>
2025-02-16 15:57:24 +0200

    explicity state the return value of the function

--- frontend/src/components/utils.js
+++ frontend/src/components/utils.js
@@ -1,3 +1,7 @@
+import React from "react";
+import { Grid2, Button, Checkbox, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+
 // get tasks from api endpoint.
 export async function getTasks() {
   return fetch("/api/tasks")
@@ -7,52 +11,44 @@ export async function getTasks() {
 
 // arranges the tasks.
 export function arrangeTasks(tasks) {
-  {
-    tasks.slice(0, 3).map((task, index) => (
-      <Grid2
-        container
-        key={task.id}
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "flex-start",
-          marginBottom: "10px",
-          borderBottom: "1px solid",
-          borderColor: "whitesmoke",
-          boxShadow: "0px 2px 2px rgba(119, 136, 153, 0.5)",
-          width: "100%",
-          borderRadius: "5px",
-        }}
-      >
-        <Grid2>
-          <Checkbox
-            id={task.id}
-            checked={task.completed}
-            inputProps={{ "aria-label": "Mark task as completed" }}
-            value={task.id}
-            onChange={this.setCheckBoxState}
-          />
-        </Grid2>
-        <Grid2 sx={{ flexWrap: "wrap", textAlign: "start" }}>
-          <Typography className="task_id" variant="h6">
-            {task.title}
-          </Typography>
-        </Grid2>
-        <Grid2 sx={{ ml: "auto" }}>
-          <Button variant="text" component={Link} to={`/tasks/${task.id}/edit`}>
-            EDIT
-          </Button>
-          <Button
-            variant="text"
-            onClick={() => {
-              this.deleteTask(task.id);
-            }}
-            sx={{ color: "red" }}
-          >
-            DELETE
-          </Button>
-        </Grid2>
+  return tasks.map((task, index) => (
+    <Grid2
+      container
+      key={task.id}
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "flex-start",
+        marginBottom: "10px",
+        borderBottom: "1px solid",
+        borderColor: "whitesmoke",
+        boxShadow: "0px 2px 2px rgba(119, 136, 153, 0.5)",
+        width: "100%",
+        borderRadius: "5px",
+      }}
+    >
+      <Grid2>
+        <Checkbox
+          id={task.id}
+          checked={task.completed}
+          inputProps={{ "aria-label": "Mark task as completed" }}
+          value={task.id}
+          onChange={() => {}}
+        />
       </Grid2>
-    ));
-  }
+      <Grid2 sx={{ flexWrap: "wrap", textAlign: "start" }}>
+        <Typography className="task_id" variant="h6">
+          {task.title}
+        </Typography>
+      </Grid2>
+      <Grid2 sx={{ ml: "auto" }}>
+        <Button variant="text" component={Link} to={`/tasks/${task.id}/edit`}>
+          EDIT
+        </Button>
+        <Button variant="text" onClick={() => {}} sx={{ color: "red" }}>
+          DELETE
+        </Button>
+      </Grid2>
+    </Grid2>
+  ));
 }
