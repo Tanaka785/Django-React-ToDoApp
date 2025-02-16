fbddbb61a4d7157519561804e03145ed6b8a5a15
Tanaka785 <tanakachanetsa204@gmail.com>
2025-02-16 12:16:51 +0200

    styles the grid and the heading

--- frontend/src/components/Tasks.js
+++ frontend/src/components/Tasks.js
@@ -2,19 +2,17 @@ import { Typography, Grid2 } from "@mui/material";
 import React, { Component } from "react";
 
 export default class Tasks extends Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return (
-            <Grid2 container spacing={1} direction={"column"}>
-                <Grid2>
-                    <Typography>
-                        Active 
-                    </Typography>
-                </Grid2>
-            </Grid2>
-        )
-    }
+  render() {
+    return (
+      <Grid2 container spacing={1} direction={"column"} sx={{ width: "100%" }}>
+        <Grid2 xs={12} sx={{ textAlign: "center" }}>
+          <Typography variant="h4">Active</Typography>
+        </Grid2>
+      </Grid2>
+    );
+  }
 }
