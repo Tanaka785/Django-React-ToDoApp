cc8461937c0e7d7c5e8302a6ee58c5d96c21abb9
Tanaka785 <tanakachanetsa204@gmail.com>
2025-02-16 15:51:54 +0200

    made required imports

--- frontend/src/components/Tasks.js
+++ frontend/src/components/Tasks.js
@@ -34,54 +34,13 @@ function Tasks() {
 
   return (
     <Grid2 container spacing={1} direction={"column"} sx={{ width: "100%" }}>
-      <Grid2 xs={12} sx={{ backgroundColor: 'primary.main', padding: '10px'}}>
-        <Typography variant="h4" sx={{ color: 'white'}}>{state.title} Tasks</Typography>
+      <Grid2 xs={12} sx={{ backgroundColor: "primary.main", padding: "10px" }}>
+        <Typography variant="h4" sx={{ color: "white" }}>
+          {state.title} Tasks
+        </Typography>
       </Grid2>
       <Grid2 xs={12} sx={{ textAlign: "center" }}>
-        {state.tasks.map((task, index) => (
-          <Grid2
-            container
-            key={task.id}
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "flex-start",
-              marginBottom: "10px",
-              borderBottom: "1px solid",
-              borderColor: "whitesmoke",
-              boxShadow: "0px 2px 2px rgba(119, 136, 153, 0.5)",
-              width: "100%",
-              borderRadius: "5px",
-            }}
-          >
-            <Grid2>
-              <Checkbox
-                id={task.id}
-                checked={task.completed}
-                inputProps={{ "aria-label": "Mark task as completed" }}
-                value={task.id}
-                onChange={() => {}}
-              />
-            </Grid2>
-            <Grid2 sx={{ flexWrap: "wrap", textAlign: "start" }}>
-              <Typography className="task_id" variant="h6">
-                {task.title}
-              </Typography>
-            </Grid2>
-            <Grid2 sx={{ ml: "auto" }}>
-              <Button
-                variant="text"
-                component={Link}
-                to={`/tasks/${task.id}/edit`}
-              >
-                EDIT
-              </Button>
-              <Button variant="text" onClick={() => {}} sx={{ color: "red" }}>
-                DELETE
-              </Button>
-            </Grid2>
-          </Grid2>
-        ))}
+        {utils.arrangeTasks(state.tasks)}
       </Grid2>
       <Grid2 xs={12} sx={{ textAlign: "center" }}>
         <Typography variant="h6" component={Link} to="/">
